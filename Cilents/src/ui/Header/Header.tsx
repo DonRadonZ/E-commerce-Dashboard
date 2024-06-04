ecc7332bda8b2492cfa5521ab6b5c41ccc9eb0a4
RadonZ <supachai27102000@gmail.com>
2024-06-04 18:03:15 +0700

    add: Heading prop

--- Cilents/src/ui/Header/Header.tsx
+++ Cilents/src/ui/Header/Header.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+    background-color: var(--color-gray-0);
+    padding: 1.2rem 4.8rem;
+    border-bottom: 1px solid var(--color-gray-100);
+    
+    display: flex;
+    gap: 2.4rem;
+    align-items: center;
+    justify-content: flex-end;
+`
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <StyledHeader>
+        Header
+    </StyledHeader>
   )
 }
