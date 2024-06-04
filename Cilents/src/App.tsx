3c7a2f836d116138b37ce6d72f68a535801add86
RadonZ <supachai27102000@gmail.com>
2024-06-04 15:16:29 +0700

    add: Route and global style

--- Cilents/src/App.tsx
+++ Cilents/src/App.tsx
@@ -1,8 +1,33 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+// Page
+import Dashboard from "./page/Dashboard";
+import Inventory from "./page/Inventory";
+import SalesReport from "./page/SalesReport";
+import Customers from "./page/Customers";
+
+
+import AppLayout from "./ui/AppLayout";
+import GlobalStyles from "./styles/GlobalStyles";
 
 export default function App() {
   return (
-    <div>
+    <>
+    <GlobalStyles />
+    <BrowserRouter>
+    <Routes>
+      <Route element={
+        <AppLayout/>
+      }>
+      <Route index element={<Navigate replace to="dashboard"/>}/>
+      <Route path="dashboard" element ={<Dashboard/>}/>
+      <Route path="salesreport" element={<SalesReport/>}/>
+      <Route path="inventory" element={<Inventory/>}/>
+      <Route path="customer" element={<Customers/>}/>
+      </Route>
+    </Routes>
       
-    </div>
+    </BrowserRouter>
+    </>
   )
 }
