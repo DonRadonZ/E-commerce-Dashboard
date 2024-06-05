310e714cf8a00428a81ebe04857e875b6a36f112
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-06-06 01:40:42 +0700

    add: database part

---
+++ Server/database/database.go
@@ -0,0 +1,11 @@
+package database
+
+// import (
+//     "context"
+//     "time"
+
+//     "go.mongodb.org/mongo-driver/mongo"
+//     "go.mongodb.org/mongo-driver/mongo/options"
+//     "go.mongodb.org/mongo-driver/mongo/readpref"
+// )
+
