88f5cc4bca2a64f9a7b422ce24e650f40118f511
RadonZ <supachai27102000@gmail.com>
2024-06-06 14:46:17 +0700

    add: new component

---
+++ Cilents/src/ui/Select.tsx
@@ -0,0 +1,39 @@
+import  { ChangeEvent } from 'react'
+import styled from 'styled-components'
+
+type StyledSelectProps = {
+    type?: string;
+}
+
+const StyledSelect = styled.select<StyledSelectProps>`
+    font-size: 1.4rem;
+    padding: 0.8rem 1.2rem;
+    border: 1px solid
+    ${(props) => 
+        props.type === "white"
+          ? "var(--color-gray-100)"
+          : "var(--color-gray-300)"
+    };
+    border-radius: var(--border-radius-sm);
+    background-color: var(--color-grey-0);
+    font-weight: 500;
+    box-shadow: var(--shadow-sm);
+`;
+
+interface SelectProps {
+    options: Array<{value: string; label: string;}>
+    value: string;
+    onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export default function Select({options,value, onChange, ...props}: SelectProps) {
+  return (
+    <StyledSelect value={value} onChange={onChange} {...props}>
+        {options.map((option) => (
+            <option value={option.value} key={option.value}>
+                {option.label}
+            </option>
+        ))}
+    </StyledSelect>
+  )
+}
