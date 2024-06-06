a4ff9fbbab81a73fecdfbaafdd15b697edeff382
RadonZ <supachai27102000@gmail.com>
2024-06-06 13:04:57 +0700

    refactor: change directory of file

--- Cilents/src/Components/layout/Sidebar/Sidebar.tsx
+++ Cilents/src/Components/layout/Sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 
 import styled from 'styled-components'
 import MainNav from './MainNav'
-import Logo from '../Logo'
+import Logo from '../../../ui/Logo'
 
 const StyledSidebar = styled.aside`
     background-color: var(--color-gray-0);
