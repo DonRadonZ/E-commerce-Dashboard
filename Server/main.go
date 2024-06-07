9b3b552c5bd55bd23788739852bb7436ccdac223
RadonZ <supachai27102000@gmail.com>
2024-06-07 14:35:04 +0700

    create: route

--- Server/main.go
+++ Server/main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"fmt"
 	
-
 	"github.com/gofiber/fiber/v2"
+	"gitlab.com/supachai27102000/e-commerce-dashboard/configs"
+	"gitlab.com/supachai27102000/e-commerce-dashboard/routes"
 )
 
 func main() {
-	fmt.Print("Hello world")
 
-	server := fiber.New()
+	app := fiber.New()
+
+	configs.ConnectDB()
+
+	routes.UserRoutes(app)
 	
-	server.Get("/healthcheck", func(c *fiber.Ctx) error {
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
 	})
 
-	server.Listen("127.0.0.1:3000")
+	app.Listen("127.0.0.1:3000")
 }
