15679e248f6d770d4080f1815b7c17b161b4964c
RadonZ <supachai27102000@gmail.com>
2024-06-07 14:38:20 +0700

    add routes

--- Server/routes/products.go
+++ Server/routes/products.go
@@ -2,6 +2,6 @@ package routes
 
 import "github.com/gofiber/fiber/v2"
 
-func CustomerRoutes(app *fiber.App){
-	app.Get("")
+func ProductRoutes(app *fiber.App){
+	
 }
--- Server/routes/routes.go
+++ Server/routes/routes.go
@@ -2,6 +2,6 @@ package routes
 
 import "github.com/gofiber/fiber/v2"
 
-func CustomerRoutes(app *fiber.App){
-	app.Get("")
+func UserRoutes(app *fiber.App) {
+	
 }
