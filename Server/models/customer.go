e5d66157bf7c4c0ccd1236091d78a5afd6b03218
RadonZ <supachai27102000@gmail.com>
2024-06-09 01:00:59 +0700

    update: change customer models

--- Server/models/customer.go
+++ Server/models/customer.go
@@ -5,6 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Customer struct {
 	Id   primitive.ObjectID `json:"id,omitempty"`
 	Name string `json:"name,omitempty"`
+	RegisterDate string  `json:"registerdate,omitempty"`
 	Email string `json:"email,omitempty"`
 	Phone string `json:"phone,omitempty"`
 }
