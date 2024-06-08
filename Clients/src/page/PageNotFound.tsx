5577ba32608d181019827c730f72912ad8ec9357
RadonZ <supachai27102000@gmail.com>
2024-06-09 01:02:21 +0700

    add page not found

--- Clients/src/page/PageNotFound.tsx
+++ Clients/src/page/PageNotFound.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import styled from "styled-components"
+import Heading from "../Components/layout/Header/Heading";
+import Button from "../Components/UI/Button/Button";
+import { useMoveBack } from '../hooks/Settings/useMoveBack';
+
+const StyledPageNotFound = styled.main`
+  height: 100vh;
+  background-color: var(--color-gray-500);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 4.8rem;
+`;
+
+const Box = styled.div`
+  
+  background-color: var(--color-gray-0);
+  border: 1px solid var(--color-gray-100);
+  border-radius: var(--border-radius-md);
+
+  & h1 {
+    margin-bottom: 3.2rem;
+  }
+`;
+
 
 export default function PageNotFound() {
+  const moveBack = useMoveBack
+
   return (
-    <div>PageNotFound</div>
+    <StyledPageNotFound>
+      <Box>
+        <Heading as="h1">
+        The page you are looking for could not be found 😢
+        </Heading>
+        <Button onClick={moveBack} size="large">
+          &larr; Go back
+        </Button>
+      </Box>
+    </StyledPageNotFound>
   )
 }
