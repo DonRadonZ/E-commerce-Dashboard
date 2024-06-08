c4f74a8fe2147c8d6a5bbc90df8cb27a6c33fb29
RadonZ <supachai27102000@gmail.com>
2024-06-09 01:01:52 +0700

    create: table part

--- Clients/src/feature/Dashboard/SalesChart.tsx
+++ Clients/src/feature/Dashboard/SalesChart.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 import DashboardBox from "./DashboardBox";
+import Heading from "../../Components/layout/Header/Heading";
+import {
+    ResponsiveContainer
+} from "recharts";
+import { format } from "date-fns";
 
 const StyledSalesChart = styled(DashboardBox)`
     grid-column: 1/-1;
@@ -9,3 +14,19 @@ const StyledSalesChart = styled(DashboardBox)`
         stroke: var(--color-gray-300)
     }
 `
+
+function SalesChart(){
+    return(
+        <StyledSalesChart>
+            <Heading as="h2">
+                Sales from {format(allDates.at(0) as any, "MMM dd yyyy")}{" "}
+                &mdash; {format(allDates.at(-1) as any, "MMM dd yyyy")}{" "}
+            </Heading>
+        <ResponsiveContainer height={300} width="100%">
+
+        </ResponsiveContainer>
+        </StyledSalesChart>
+    )
+}
+
+export default SalesChart;
--- Clients/src/feature/Inventory/InventoryTable.tsx
+++ Clients/src/feature/Inventory/InventoryTable.tsx
@@ -6,10 +6,10 @@ export default function CustomerTable({count}) {
   return (
     <Table columns="2.4fr 2.4fr 1.4fr 1.2fr 3.2rem">
         <Table.Header>
-            <div>Registeration Date</div>
             <div>Name</div>
-            <div>E-mail</div>
-            <div>Total Buy</div>
+            <div>Category</div>
+            <div>Amount</div>
+            <div>Status</div>
         </Table.Header>
 
         <Table.Footer>
