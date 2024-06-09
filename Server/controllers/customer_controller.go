c631207a1baf3b40f6279cbcdb4ca7c2fdace02b
RadonZ <supachai27102000@gmail.com>
2024-06-10 00:07:24 +0700

    remove: unused route

--- Server/controllers/customer_controller.go
+++ Server/controllers/customer_controller.go
@@ -18,35 +18,7 @@ import (
 var customerCollection *mongo.Collection = configs.GetCollection(configs.DB, "customers")
 var customervalidate = validator.New()
 
-func AddCustomer(c *fiber.Ctx) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	var customer models.Customer
-	defer cancel()
 
-	if err := c.BodyParser(&customer); err != nil {
-		return c.Status(http.StatusBadRequest).JSON(responses.Response{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
-	}
-
-	if validationErr := customervalidate.Struct(&customer); validationErr != nil {
-		return c.Status(http.StatusBadRequest).JSON(responses.Response{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": validationErr.Error()}})
-	}
-
-	newCustomer := models.Customer{
-		Id: primitive.NewObjectID(),
-		Name: customer.Name,
-		RegisterDate: customer.RegisterDate,
-		Email: customer.Email,
-		Phone: customer.Phone,
-	}
-
-	result, err := customerCollection.InsertOne(ctx, newCustomer)
-	if err != nil {
-		c.Status(http.StatusInternalServerError).JSON(responses.Response{Status: fiber.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
-		
-	}
-	
-	return c.Status(http.StatusCreated).JSON(responses.Response{Status: http.StatusCreated, Message: "success", Data: &fiber.Map{"data": result}})
-}
 
 func GetACustomer(c *fiber.Ctx) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -66,44 +38,49 @@ func GetACustomer(c *fiber.Ctx) {
 	 c.Status(http.StatusOK).JSON(responses.Response{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": customer}})
 }
 
-func EditACustomer(c *fiber.Ctx){
+func EditACustomer (c*fiber.Ctx) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	customerId := c.Params("customerId")
-
 	var customer models.Customer
 	defer cancel()
-
 	objId, _ := primitive.ObjectIDFromHex(customerId)
 
-	if err := c.BodyParser(&customer); err != nil {
-		c.Status(http.StatusBadRequest).JSON(responses.Response{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
-		return
-	}
-	if validationErr := inventoryvalidate.Struct(&customer); validationErr != nil {
-		c.Status(http.StatusBadRequest).JSON(responses.Response{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": validationErr.Error()}})
-		return
+	if err := c.BodyParser(&customer); err!= nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.Response{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
 	}
 
-	update := bson.M{"Name":customer.Name, "registerdate":customer.RegisterDate, "email":customer.Email, "phone":customer.Phone}
+	if validationErr := customervalidate.Struct(&customer); validationErr != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.Response{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": validationErr.Error()}})
+	}
 
-	result, err := customerCollection.UpdateOne(ctx, bson.M{"id": objId}, bson.M{"$set": update})
+	update := bson.M{
+	"Name": customer.Name,
+	"Email": customer.Email,
+	"Phone": customer.Phone,
+	"Purchase_Amount": customer.Purchase_amount,
+}
+	result, err := customerCollection.UpdateOne(ctx, bson.M{"id": objId},bson.M{"$set": update})
 
 	if err != nil {
-		c.Status(http.StatusInternalServerError).JSON(responses.Response{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
-		return
+		return c.Status(http.StatusInternalServerError).JSON(
+			responses.Response{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}},
+		)
 	}
-	
+
 	var updatedCustomer models.Customer
 	if result.MatchedCount == 1 {
 		err := customerCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&updatedCustomer)
 
 		if err != nil {
-			c.Status(http.StatusInternalServerError).JSON(responses.Response{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
-			return
+			return c.Status(http.StatusInternalServerError).JSON(responses.Response{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
 		}
+	}
+
+	return c.Status(http.StatusOK).JSON(responses.Response{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": updatedCustomer}})
+	
 }
-	c.Status(http.StatusOK).JSON(responses.Response{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": updatedCustomer}})
-}
+
+
 
 func DeleteACustomer(c *fiber.Ctx) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -112,7 +89,7 @@ func DeleteACustomer(c *fiber.Ctx) error {
 
 	objId, _ := primitive.ObjectIDFromHex(customerId)
 
-	result, err := inventoryCollection.DeleteOne(ctx, bson.M{"id": objId})
+	result, err := customerCollection.DeleteOne(ctx, bson.M{"id": objId})
 
 	if err != nil {
 		return c.Status(http.StatusInternalServerError).JSON(responses.Response{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
@@ -150,7 +127,7 @@ func GetAllCustomer(c *fiber.Ctx) error {
 
 		customers = append(customers, singleCustomer)
 	}
-	return c.Status(http.StatusOK).JSON(
+		return c.Status(http.StatusOK).JSON(
 		responses.Response{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": customers}},
 	)	
 }
