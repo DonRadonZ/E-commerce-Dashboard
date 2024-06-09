b37046366ea3daea6717ccd64715160187a737b9
RadonZ <supachai27102000@gmail.com>
2024-06-10 00:04:15 +0700

    add react query and dark mode

--- Clients/src/App.tsx
+++ Clients/src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // Page
 import Dashboard from "./page/Dashboard";
@@ -11,9 +13,23 @@ import AppLayout from "./Components/layout/AppLayout";
 import GlobalStyles from "./styles/GlobalStyles";
 import Product from "./page/Product";
 
+
+import DarkModeProvider from "./contexts/DarkModeContext";
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    }
+  }
+})
+
 export default function App() {
   return (
-    <>
+    <DarkModeProvider>
+    <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
     <GlobalStyles />
     <BrowserRouter>
     <Routes>
@@ -30,6 +46,7 @@ export default function App() {
     </Routes>
       
     </BrowserRouter>
-    </>
+    </QueryClientProvider>
+    </DarkModeProvider>
   )
 }
