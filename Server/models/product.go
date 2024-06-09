af67ff2235b64268fbdac1c8728f015659912b52
RadonZ <supachai27102000@gmail.com>
2024-06-10 00:11:25 +0700

    update: modal photo

--- Server/models/product.go
+++ Server/models/product.go
@@ -1,5 +1,10 @@
 package models
 
 type Product struct{
-	Photo string `json:"inventory_name,omitempty" valiadte:"required"`
+	Photo string `json:"product_photo,omitempty" valiadte:"required"`
+	Product_Name string `json:"product_name,omitempty" valiadte:"required"`
+	Price float64 `json:"price,omitempty" valiadte:"required"`
+	Discount float64 `json:"discount,omitempty" valiadte:"required"`
+	Amount_Sale float64 `json:"amount_sale,omitempty" valiadte:"required"`
+	Circulation float64 `json:"circulation,omitempty" valiadte:"required"`
 }
