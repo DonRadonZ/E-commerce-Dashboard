8318bb940d50b9e96f580e98f60f44b86b0964be
RadonZ <supachai27102000@gmail.com>
2024-06-13 02:00:01 +0700

    add: status part on table

--- Clients/src/feature/Inventory/InventoryRow.tsx
+++ Clients/src/feature/Inventory/InventoryRow.tsx
@@ -1,5 +1,9 @@
 import styled from "styled-components"
 import Table from "../../Components/UI/Table/Table";
+import Modal from "../../Components/UI/Modal";
+import Menus from "../../Components/UI/Menus";
+import { HiPencil } from "react-icons/hi2";
+
 
 const Inventory = styled.div`
     font-size: 1.6rem;
@@ -20,10 +24,32 @@ const Amount = styled.div`
   font-weight: 600;
 `;
 
+const Instock = styled.div`
+  font-family: "Sarabun";
+  font-weight: 600;
+  color: green;
+  
+`;
+
+const Lowstock = styled.div`
+  font-family: "Sarabun";
+  font-weight: 600;
+  color: orange;
+  
+`;
+
+const OutofStock = styled.div`
+  font-family: "Sarabun";
+  font-weight: 600;
+  color: red;
+  
+`;
+
 // const Status = 
 
 export interface ICustomer  {
   inventory : {
+    inventory_id: string,
     inventory_name: string;
   category: string;
   remainder: number;
@@ -33,16 +59,45 @@ export interface ICustomer  {
 export default function InventoryRow({inventory}: ICustomer) {
 
   const {
+    inventory_id,
     inventory_name,
     category,
-    remainder
+    remainder,
+    
   } = inventory
+
+  // const statusToTagName: any = {
+  //   instock: "blue",
+  //   lowstock: "green",
+  //   outofstock: "red"
+  // }
 
   return (
     <Table.Row>
         <Inventory>{inventory_name}</Inventory>
         <Category>{category}</Category>
-        <Amount>{remainder}</Amount>
+        { remainder ?
+        <Amount>{remainder}</Amount> : <Amount>0</Amount>
+  
+        }
+        {remainder > 9 ? <Instock>In Stock </Instock>: remainder > 0 && remainder <= 5 ? <Lowstock>Low Stock</Lowstock>: <OutofStock>Out of Stock</OutofStock>}
+        <div>
+       <Modal>
+          <Menus.Menu>
+            <Menus.Menu>
+              <Menus.Toggle id={inventory_id}/>
+                <Menus.List id={inventory_id}>
+                  <Menus.Button
+                  icon={<HiPencil />}
+                  >
+                    Edit
+                  </Menus.Button>
+                </Menus.List>
+            
+            </Menus.Menu>
+          </Menus.Menu>
+       </Modal>
+      </div>
     </Table.Row>
   )
 }
