fb057e3c9e9ca9333491e900a6ba19ccc4cb00df
RadonZ <supachai27102000@gmail.com>
2024-06-13 02:03:05 +0700

    update sales report operation

--- Clients/src/feature/SalesReport/SalesReportTableOperations.tsx
+++ Clients/src/feature/SalesReport/SalesReportTableOperations.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import TableOperations from "../../Components/UI/Table/TableOperations";
+import SearchFilter from "../../Components/utils/SearchFilter";
+import SortBy from "../../Components/utils/SortBy";
+
 
 export default function SalesReportTableOperations() {
   return (
-    <div>SalesReportTableOperations</div>
+    <TableOperations>
+    <SearchFilter placeholder="Search..."/>
+    <SortBy
+       options={[
+        {
+          value: "recent-desc",
+          label: "Sort by date (recent first)"
+        },
+        {
+          value: "recent-asc",
+          label: "Sort by date (older first)"
+        },
+        {
+          value: "totalPrice-desc",
+          label: "Sort by amount (high first)"
+        },
+        {
+          value: "totalPrice-asc",
+          label: "Sort by amount (low first)"
+        },
+       ]} 
+    />
+      </TableOperations>
   )
 }
