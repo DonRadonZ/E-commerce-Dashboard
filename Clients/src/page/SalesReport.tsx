0019575fe051bdf2b731f1dad5b6f8b6b252479e
RadonZ <supachai27102000@gmail.com>
2024-06-13 02:05:01 +0700

    add Sales Report operation to page

--- Clients/src/page/SalesReport.tsx
+++ Clients/src/page/SalesReport.tsx
@@ -1,6 +1,7 @@
 import Heading from "../Components/layout/Header/Heading";
 import Row from "../Components/layout/Row";
 import SalesReportTable from "../feature/SalesReport/SalesReportTable";
+import SalesReportTableOperations from "../feature/SalesReport/SalesReportTableOperations";
 
 
 export default function SalesReport() {
@@ -8,6 +9,8 @@ export default function SalesReport() {
     <>
     <Row type="horizontal">
       <Heading as="h1">Sales Report</Heading>
+      
+      <SalesReportTableOperations/>
       </Row>
       <Row>
         <SalesReportTable/>
